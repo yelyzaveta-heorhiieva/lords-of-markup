a0f4c77653e438117678573ae91e35f9c722f05b
Yelyzaveta Heorhiieva <heorhiieva.y@gmail.com>
2025-01-06 17:42:17 +0200

    add js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,69 @@
-const open =
-    document.querySelector('.burger-btn');
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
-    const close =
-    document.querySelector('.mobile-btn');
+const openMenu = document.querySelector('.burger-btn');
+const closeMenu = document.querySelector('.mobile-btn');
+const menu = document.querySelector('.mobile-menu');
+const navList = document.querySelector('.nav-list-header');
+const links = document.querySelectorAll('.nav-link-header');
+const catalogBtn = document.querySelector('.catalog-btn');
+const catalogItem = document.querySelectorAll('.last')
 
 
-const menu =
-    document.querySelector('.mobile-menu');
 
-open.addEventListener('click',
-    function () {
-        menu.classList.add('is-open');
-    });
+openMenu.addEventListener('click', () => menu.classList.add('is-open'));
+closeMenu.addEventListener('click', () => menu.classList.remove('is-open'));
 
-    close.addEventListener('click',
-    function () {
-        menu.classList.remove('is-open');
-    });
+navList.addEventListener('click', handleClick);
+menu.addEventListener('click', handleClick);
+
+function handleClick(event) {
+    if (event.target.classList.contains('nav-link-header')) {
+        [...links].forEach(link => link.classList.remove('current'));
+        event.target.classList.add('current');
+    }
+}
+
+catalogBtn.addEventListener('click', showMore)
+
+function showMore() {
+    [...catalogItem].forEach(item => {
+        item.classList.toggle('hide')
+        if (item.classList.contains('hide')) {
+           catalogBtn.textContent = "Show more"; 
+        } else {
+          catalogBtn.textContent = "Hide";  
+        }
+    })     
+}
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  breakpoints: {
+    320: {
+          slidesPerView: 1,
+    },
+    768: {
+        slidesPerView: 2,
+        spaceBetween: 16,
+    },
+   1280: {
+       slidesPerView: 3,
+       spaceBetween: 20,
+    }
+  },
+
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+  mousewheel: {
+      invert: true,
+  },
+  
+   keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+});
